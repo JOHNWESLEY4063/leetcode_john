9ae2c9c502935de777dfc91d84bd70791c9eb711
JOHNWESLEY4063 <166008497+JOHNWESLEY4063@users.noreply.github.com>
2024-09-16 21:40:34 +0530

    Add files via upload

---
+++ 539. minimum time diff(day 13).java
@@ -0,0 +1,24 @@
+import java.util.Collections;
+import java.util.List;
+
+class Solution{
+    public int findMinDifference(List<String> timePoints){
+List<Integer> minutesList=new ArrayList<>();
+    for(String time : timePoints){
+        String[] parts=time.split(":");
+        int hours=Integer.parseInt(parts[0]);
+        int minutes=Integer.parseInt(parts[1]);
+        minutesList.add(hours * 60 + minutes);
+    }
+    Collections.sort(minutesList);
+    int minDifference=Integer.MAX_VALUE;
+    for(int i=1;i<minutesList.size();i++){
+        int diff=minutesList.get(i)-minutesList.get(i-1);
+        minDifference=Math.min(minDifference,diff);
+    }
+    int circularDiff=1440-minutesList.get(minutesList.size()-1)+minutesList.get(0);
+    minDifference=Math.min(minDifference,circularDiff);
+    return minDifference;
+
+    }
+}
